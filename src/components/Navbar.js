7a5b56ae163b311d83d14add2f8010ad1a2af3ba
lakshit1504 <121532634+lakshit1504@users.noreply.github.com>
2023-05-24 17:27:50 +0530

    change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,9 +17,9 @@ export default function Navbar() {
   let location = useLocation();
 
   return (
-    <div><nav className="navbar navbar-expand-lg navbar-dark bg-warning">
+    <div><nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
-      <Link className="navbar-brand" to="/"><span style={{"color":"black"}}>My</span><span style={{"color":"#ff4646"}}>Notes</span></Link>
+      <Link className="navbar-brand" to="/"><span style={{"color":"white"}}>My</span><span style={{"color":"#ff4646"}}>Notes</span></Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
